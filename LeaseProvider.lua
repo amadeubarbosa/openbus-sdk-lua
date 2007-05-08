c17ef49e66705920fe4a188af7e616edae89df04
Cristina Ururahy <ururahy@tecgraf.puc-rio.br>
2007-05-08 18:39:48 +0000

    1) O leaseProvider passa a verificar se o lease está válido quando chega um pedido de renew. Se não estiver, faz logout. 2) De tempos em tempos verifica se há algum lease expirado. Se houver, faz logout.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/src/lua/openbus/common@63297 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ LeaseProvider.lua
@@ -0,0 +1,43 @@
+-----------------------------------------------------------------------------
+-- Objeto que é responsável por verificar os leases oferecidos
+--
+-- Última alteração:
+--   $Id:$
+-----------------------------------------------------------------------------
+
+local oil = require"oil"
+local oop = require "loop.base"
+local Timer = require "loop.thread.Timer"
+local log = require "openbus.common.Log"
+
+module ("openbus.common.LeaseProvider", oop.class)
+
+-- Constrói o provider
+function __init(self, checkLeases, rate)
+  local timer = Timer{
+    scheduler = oil.scheduler,
+    action = checkLeases,
+    rate = rate,
+  }
+  local newObj = oop.rawnew(self,
+    { timer = timer,
+    })
+  newObj:startCheck()
+  return newObj
+end
+
+function seCheckLeasesFunction(self, checkLeases)
+  self.timer.action = checkLeases
+end
+
+function setRate(self, rate)
+  self.timer.rate = rate
+end
+
+function startCheck(self)
+  self.timer:enable()
+end
+
+function stopCheck(self)
+  self.timer:disable()
+end
