2f757d3aa0ec0bc7a8f0d8806831baed5a797566
Cristina Ururahy <ururahy@tecgraf.puc-rio.br>
2007-06-14 19:02:00 +0000

    (OPENBUS-15) Implementação do mecanismo de "callback" para falha da renovação de "lease" de credencial em Lua.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/src/lua/openbus/common@64476 ae0415b3-e90b-0410-900d-d0be9363c56b

--- LeaseHolder.lua
+++ LeaseHolder.lua
@@ -10,15 +10,18 @@ local oop = require "loop.base"
 local Timer = require "loop.thread.Timer"
 local log = require "openbus.common.Log"
 
+local tostring = tostring
+
 module ("openbus.common.LeaseHolder", oop.class)
 
 -- Constrói o holder
-function __init(self, lease, credential, leaseProvider)
+function __init(self, lease, credential, leaseProvider, leaseExpiredCallback)
   log:lease("Lease set to "..lease)
   return oop.rawnew(self,
     { lease = lease,
       credential = credential,
       provider = leaseProvider,
+      leaseExpiredCallback = leaseExpiredCallback, -- opcional
     })
 end
 
@@ -48,10 +51,26 @@ function startRenew(self)
     }
     function timer.action(timer)
       -- self é o leaseHolder
-      local granted, newlease =
-        self:getProvider():renewLease(self.credential)
+      local provider = self:getProvider()
+      local success, granted, newlease  =
+        oil.pcall(provider.renewLease, provider, self.credential)
+      if not success then
+        -- Quando ocorre falha no acesso ao provedor, tenta renovar com
+        -- uma freqüência maior.
+        log:warn("Falha na acessibilidade ao provedor do lease.")
+--      log:warn("Falha na acessibilidade ao provedor do lease:"..
+--        tostring(granted).."\n")
+        timer.rate = (self.retrying and timer.rate) or (timer.rate /2)
+        self.retrying = true
+        return
+      end
+      self.retrying = false
       if not granted then
+        log:lease("Lease não renovado.")
         timer:disable()
+        if self.leaseExpiredCallback then
+          self:leaseExpiredCallback()
+        end
         return
       end
       if timer.rate ~= newlease then
