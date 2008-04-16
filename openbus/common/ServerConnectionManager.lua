5f5b178cf57261b2bb7b0ab3c651cf7ce698fd26
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2008-04-16 00:02:15 +0000

    Classe ServiceConnectionManager renomeada para ServerConnectionManager.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core/utilities/lua@75044 ae0415b3-e90b-0410-900d-d0be9363c56b

--- openbus/common/ServerConnectionManager.lua
+++ openbus/common/ServerConnectionManager.lua
@@ -10,7 +10,7 @@ local ConnectionManager = require "openbus.common.ConnectionManager"
 --Gerenciador de conexões e desconexões de serviços ao barramento (autenticados
 --por certificado).
 ---
-module "openbus.common.ServiceConnectionManager"
+module "openbus.common.ServerConnectionManager"
 
 oop.class(_M, ConnectionManager)
 
@@ -49,14 +49,14 @@ function connect(self, name, leaseExpiredCallback)
   end
   local challenge = accessControlService:getChallenge(name)
   if not challenge then
-    log:error("ServiceConnectionManager: o desafio para "..name..
+    log:error("ServerConnectionManager: o desafio para "..name..
               " não foi obtido junto ao Servico de Controle de Acesso.")
     return false
   end
   local privateKey, errorMessage = 
     lce.key.readprivatefrompemfile(self.privateKeyFile)
   if not privateKey then
-    log:error("ServiceConnectionManager: erro obtendo chave privada para "..
+    log:error("ServerConnectionManager: erro obtendo chave privada para "..
               name.." em "..  self.privateKeyFile..": "..errorMessage)
     return false
   end
@@ -68,7 +68,7 @@ function connect(self, name, leaseExpiredCallback)
   local success, credential, lease =
     accessControlService:loginByCertificate(name, answer)
   if not success then
-    log:error("ServiceConnectionManager: insucesso no login de "..name)
+    log:error("ServerConnectionManager: insucesso no login de "..name)
     return false
   end
   self:completeConnection(credential, lease,
@@ -80,13 +80,13 @@ end
 --Reconecta o cliente após uma expiração de lease.
 ---
 function reconnect(self)
-  log:conn("ServiceConnectionManager: reconectando "..self.name)
+  log:conn("ServerConnectionManager: reconectando "..self.name)
   if self:connect(self.name, self.leaseExpiredCallback) then
-    log:conn("ServiceConnectionManager: "..self.name.." reconectado")
+    log:conn("ServerConnectionManager: "..self.name.." reconectado")
     if self.leaseExpiredCallback then
       self.leaseExpiredCallback()
     end
   else
-    log:error("ServiceConnectionManager: insucesso na reconexão de "..self.name)
+    log:error("ServerConnectionManager: insucesso na reconexão de "..self.name)
   end
 end
