2f9f83365bcab31908669f007f4679860afd05a2
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2009-06-22 20:43:06 +0000

    [OPENBUS-245] Adicao de definicoes sobre as interfaces dos servicos basicos.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core/utilities/lua@92699 ae0415b3-e90b-0410-900d-d0be9363c56b

--- openbus/util/Utils.lua
+++ openbus/util/Utils.lua
@@ -12,6 +12,44 @@ local tostring = tostring
 module "openbus.util.Utils"
 
 ---
+--  A interface IAccessControlService.
+---
+ACCESS_CONTROL_SERVICE_INTERFACE =
+  "IDL:openbusidl/acs/IAccessControlService:1.0"
+
+---
+--  A interface ILeaseProvider.
+---
+LEASE_PROVIDER_INTERFACE = "IDL:openbusidl/acs/ILeaseProvider:1.0"
+
+---
+--  As chaves CORBALOC para obtenção das interfaces do ACS.
+---
+ICOMPONENT_KEY = "/IC"
+ACCESS_CONTROL_SERVICE_KEY = "/ACS"
+LEASE_PROVIDER_KEY = "/LP"
+
+---
+--  A interface ISessionService.
+---
+SESSION_SERVICE_INTERFACE = "IDL:openbusidl/ss/ISessionService:1.0"
+
+---
+--  O nome da faceta do Serviço de Sessão.
+---
+SESSION_SERVICE_FACET_NAME = "sessionService"
+
+---
+--  A interface IHDataService.
+---
+DATA_SERVICE_INTERFACE = "IDL:openbusidl/data_service/IHDataService:1.0"
+
+---
+--  Nome da propriedade que indica o identificador de um componente.
+---
+COMPONENT_ID_PROPERTY_NAME = "component_id"
+
+---
 -- Obtém referências para as principais facetas do Serviço de Controle de
 -- Acesso.
 --
@@ -25,18 +63,18 @@ module "openbus.util.Utils"
 --
 function fetchAccessControlService(orb, host, port)
   port = tostring(port)
-  local acs = orb:newproxy("corbaloc::".. host .. ":" .. port .. "/ACS",
-                           "IDL:openbusidl/acs/IAccessControlService:1.0")
+  local acs = orb:newproxy("corbaloc::".. host .. ":" .. port ..
+    ACCESS_CONTROL_SERVICE_KEY, "IDL:openbusidl/acs/IAccessControlService:1.0")
   if acs:_non_existent() then
     log:error("Utils: Faceta IAccessControlService não encontrada.")
   end
-  local lp = orb:newproxy("corbaloc::".. host .. ":" .. port .. "/LP",
-                           "IDL:openbusidl/acs/ILeaseProvider:1.0")
+  local lp = orb:newproxy("corbaloc::".. host .. ":" .. port ..
+    LEASE_PROVIDER_KEY, "IDL:openbusidl/acs/ILeaseProvider:1.0")
   if lp:_non_existent() then
     log:error("Utils: Faceta ILeaseProvider não encontrada.")
   end
-  local ic = orb:newproxy("corbaloc::".. host .. ":" .. port .. "/IC",
-                           "IDL:scs/core/IComponent:1.0")
+  local ic = orb:newproxy("corbaloc::".. host .. ":" .. port .. ICOMPONENT_KEY,
+    "IDL:scs/core/IComponent:1.0")
   if ic:_non_existent() then
     log:error("Utils: Faceta IComponent não encontrada.")
   end
