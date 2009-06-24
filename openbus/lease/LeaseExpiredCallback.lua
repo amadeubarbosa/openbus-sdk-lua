8259ec189adb2628d65be55ac13d28ae230d5b7a
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2009-06-24 00:08:46 +0000

    [OPENBUS-252] Funcionalidade para adicao de mais de uma callback para lease expirado, seguindo o modelo da API Java.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/core/utilities/lua@92836 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ openbus/lease/LeaseExpiredCallback.lua
@@ -0,0 +1,60 @@
+-- $Id: LeaseExpiredCallback.lua $
+
+local oop = require "loop.base"
+local set = require "loop.collection.UnorderedArraySet"
+
+---
+-- API de adição de callbacks para expiração de lease.
+---
+module ("openbus.lease.LeaseExpiredCallback", oop.class)
+
+---
+-- Construtor.
+---
+function __init(self)
+  return oop.rawnew(self, { callbacks = set() })
+end
+
+---
+-- O <i>lease</i> expirou e não será mais renovado pelo {@link LeaseRenewer}
+---
+function expired(self)
+  if #(self.callbacks) == 0 then
+    return
+  end
+  local i = 1
+  repeat
+    self.callbacks[i]()
+    i = i + 1
+  until i > #(self.callbacks)
+end
+
+---
+-- Adiciona um observador para receber eventos de expiração do <i>lease</i>.
+--
+-- @param lec O observador.
+--
+-- @return {@code true}, caso o observador seja adicionado, ou {@code false}
+--         , caso contrário.
+---
+function addLeaseExpiredCallback(self, lec)
+  if self.callbacks:add(lec) then
+    return true
+  end
+  return false
+end
+
+---
+-- Remove um observador de expiração do <i>lease</i>.
+--
+-- @param lec O observador.
+--
+-- @return {@code true}, caso o observador seja removido, ou {@code false},
+--         caso contrário.
+---
+function removeLeaseExpiredCallback(self, lec)
+  if self.callbacks:remove(lec) then
+    return true
+  end
+  return false
+end
