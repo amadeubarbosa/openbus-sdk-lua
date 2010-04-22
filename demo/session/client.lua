229053944c6863715219c04a2c053b6d80af2d66
Rodrigo Carneiro Henrique <rodrigoh@tecgraf.puc-rio.br>
2010-04-22 23:39:23 +0000

    [OPENBUS-689]: Migrar a API Lua de core/utilities/lua para sdk/lua
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/lua/trunk@104598 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ demo/session/client.lua
@@ -0,0 +1,114 @@
+-------------------------------------------------------------------------------
+-- Demo de eventos do Serviço de Sessão
+-- client.lua
+--
+
+local oo      = require "loop.base"
+local oil     = require "oil"
+local utils   = require ("scs.core.utils").Utils()
+local openbus = require "openbus.Openbus"
+
+--oil.verbose:level(3)
+
+-- Carrega as propriedades
+local props = {}
+utils:readProperties(props, "HelloSession.properties")
+-- Aliases
+local host               = props["host.name"].value
+local port               = tonumber(props["host.port"].value)
+local login              = props["login"].value
+local password           = props["password"].value
+local privateKeyFile     = props["private.key"].value
+local acsCertificateFile = props["acs.certificate"].value
+
+-- Inicialização
+openbus:init(host, port)
+-- ORB deve estar inicializado antes de carregar o SCS
+local scs = require "scs.core.base"
+
+-- Auxiliares
+local orb = openbus:getORB()
+local compFacet = "IDL:scs/core/IComponent:1.0"
+local sessionFacet = "IDL:tecgraf/openbus/session_service/v1_05/ISession:1.0"
+local sinkFacet = "IDL:tecgraf/openbus/session_service/v1_05/SessionEventSink:1.0"
+local context, sessionId
+
+-------------------------------------------------------------------------------
+-- Componente para os eventos
+--
+local Sink = oo.class{}
+
+function Sink:disconnect()
+  -- do nothing
+end
+
+function Sink:push(event)
+  print("Recebido: " .. event.value._anyval)
+end
+
+-------------------------------------------------------------------------------
+-- Descrições do componente
+local facetDescriptions = {}
+
+facetDescriptions.IComponent = {
+  name = "IComponent",
+  interface_name = "IDL:scs/core/IComponent:1.0",
+  class = scs.Component
+}
+
+facetDescriptions.IMetaInterface = {
+  name = "IMetaInterface",
+  interface_name = "IDL:scs/core/IMetaInterface:1.0",
+  class = scs.MetaInterface
+}
+
+facetDescriptions.SessionEventSink = {
+  name = "SessionEventSink",
+  interface_name = sinkFacet,
+  class = Sink,
+}
+
+local componentId = {
+  name = "HelloSink",
+  major_version = 1,
+  minor_version = 0,
+  patch_version = 0,
+  platform_spec = ""
+}
+
+-------------------------------------------------------------------------------
+function main ()
+  -- Permite que o ORB comece a aguardar requisições
+  openbus:run()
+  -- Instanciação do componente
+  context = scs.newComponent(facetDescriptions, {}, componentId)
+  -- Conexão com o barramento e obtenção do componente de sessão
+  local registryService = openbus:connectByLoginPassword(login, password)
+  if not registryService then
+    io.stderr:write("[ERRO] Não foi possível se conectar ao barramento.\n")
+    os.exit(1)
+  end
+  -- Assume que há só uma sessão cadastrada no registro
+  -- local offers = registryService:find({"ISession"})
+  local offers = registryService:findByCriteria({"ISession"}, { {name = "sessionName", value = {"HelloSession"}} })
+  for _, offer in ipairs(offers) do
+    local succ = offer.member.__try:_component()
+    if succ then
+      local component = orb:narrow(offer.member, compFacet)
+      local session = orb:narrow(component:getFacet(sessionFacet), sessionFacet)
+      sessionId = session:addMember(context.IComponent)
+      return
+    end
+  end
+  io.stderr:write("[ERRO] Não foi possível localizar sessão.\n")
+  openbus:disconnect()
+  openbus:destroy()
+  os.exit(1)  
+end
+
+oil.main(function()
+  local succ, msg = oil.pcall(main)
+  if not succ then
+    print(msg)
+  end
+end)
