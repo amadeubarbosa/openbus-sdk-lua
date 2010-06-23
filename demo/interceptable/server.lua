7edd980758de05f72c659ec50e7ee332cce77225
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2010-06-23 18:35:15 +0000

    [OPENBUS-812] Criar um teste Cliente/Servidor para exercitar o funcionamento do método setInterceptable().
    Os testes criados em Java exigem um cliente e um servidor, e fazem o lançamento de processos através da API Java. Como Lua não dá suporte a isto, teríamos de incluir tratamento para as diversas plataformas que suportamos. Por isso, resolvi criar uma nova demo ao invés de um teste a ser utilizado em ferramenta dependente de plataforma.
    
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/lua/trunk@107186 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ demo/interceptable/server.lua
@@ -0,0 +1,95 @@
+--
+-- OpenBus Demo relativa ao controle de interceptação fornecido pela API
+-- server.lua
+--
+
+local oil = require "oil"
+local oop = require "loop.base"
+local openbus = require "openbus.Openbus"
+local scsutils = require ("scs.core.utils").Utils()
+
+-- Inicialização do barramento
+local props = {}
+scsutils:readProperties(props, "Interceptable.properties")
+local host = props["host.name"].value
+local port = props["host.port"].value
+openbus:init(host, tonumber(port))
+
+if arg[1] == "ft" then
+  openbus:enableFaultTolerance()
+end
+
+local orb = openbus:getORB()
+
+local scs = require "scs.core.base"
+
+-- Implementação da Faceta IHello
+local Hello = oop.class {}
+function Hello:sayHello()
+  print "HELLO!\n\n"
+end
+
+-- Descrições do componente HelloComponent
+local facetDescriptions = {}
+facetDescriptions.IHello = {
+  name = "IHello",
+  interface_name = "IDL:demoidl/hello/IHello:1.0",
+  class = Hello
+}
+local componentId = {
+  name = "HelloComponent",
+  major_version = 1,
+  minor_version = 0,
+  patch_version = 0,
+  platform_spec = ""
+}
+
+-- Execução
+function main ()
+  -- Carga da IDL Hello
+  orb:loadidlfile("idl/hello.idl")
+
+  -- Permite que o ORB comece a aguardar requisições
+  openbus:run()
+
+  -- Instanciação do componente HelloComponent
+  local component = scs.newComponent(facetDescriptions, {}, componentId)
+
+  -- Coloca o metodo sayHello como nao interceptavel
+  openbus:setInterceptable(facetDescriptions.IHello.interface_name, "sayHello", false)
+
+  -- Conexão com o barramento e registro do componente
+  local entityName = props["entity.name"].value
+  local privateKeyFile = props["private.key"].value
+  local acsCertificateFile = props["acs.certificate"].value
+
+  local registryService = openbus:connectByCertificate(entityName, privateKeyFile, acsCertificateFile)
+  if not registryService then
+    io.stderr:write("HelloServer: Erro ao conectar ao barramento.\n")
+    os.exit(1)
+  end
+  local suc, id = registryService.__try:register({
+    member = component.IComponent,
+    properties = {
+      { name = "facets", value = {"IDL:demoidl/hello/IHello:1.0",} }
+    },
+  })
+  if not suc then
+      io.stderr:write("HelloServer: Erro ao registrar ofertas.\n")
+      if id[1] == "IDL:tecgraf/openbus/core/v1_05/registry_service/UnathorizedFacets:1.0" then
+        for _, facet in ipairs(id.facets) do
+          io.stderr:write(string.format("Registro da faceta '%s' não autorizado\n", facet))
+        end
+      else
+        io.stderr:write(string.format("Erro: %s.\n", id[1]))
+      end
+      os.exit(1)
+  end
+  print("*********************************************\n")
+  print("PUBLISHER\nServiço Hello registrado no barramento do OpenBus.\n")
+  print("*********************************************")
+end
+
+oil.main(function()
+  print(oil.pcall(main))
+end)
