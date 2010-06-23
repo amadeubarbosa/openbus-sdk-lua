ea68df6e3ee4ff7094ee0c5dde46fc0e0181e2e0
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2010-06-23 22:10:31 +0000

    [OPENBUS-812] Criar um teste Cliente/Servidor para exercitar o funcionamento do método setInterceptable().
    Os testes criados em Java exigem um cliente e um servidor, e fazem o lançamento de processos através da API Java. Como Lua não dá suporte a isto, teríamos de incluir tratamento para as diversas plataformas que suportamos. Por isso, resolvi criar uma nova demo ao invés de um teste a ser utilizado em ferramenta dependente de plataforma.
    
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/lua/branches@107226 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ OB_SDK_LUA_v1_05_2010_05_13/demo/interceptable/client.lua
@@ -0,0 +1,63 @@
+--
+-- OpenBus Demo
+-- client.lua
+--
+
+local oil = require "oil"
+local openbus = require "openbus.Openbus"
+local scsutils = require ("scs.core.utils").Utils()
+local log = require "openbus.util.Log"
+
+-- Inicializacao do barramento
+local props = {}
+scsutils:readProperties(props, "Interceptable.properties")
+local host = props["host.name"].value
+local port = props["host.port"].value
+openbus:init(host, tonumber(port))
+
+
+if arg[1] == "ft" then
+  openbus:enableFaultTolerance()
+end
+
+--
+local orb = openbus:getORB()
+
+-- Execução
+function main ()
+  -- Carga da IDL Hello
+  orb:loadidlfile("idl/hello.idl")
+
+  -- Conexão com o barramento e obtenção do componente HelloComponent
+  local login = props.login.value
+  local password = props.password.value
+  local registryService = openbus:connectByLoginPassword(login, password)
+  if not registryService then
+    io.stderr:write("HelloClient: Erro ao conectar ao barramento.\n")
+    os.exit(1)
+  end
+
+  local offers = openbus:getRegistryService():find({"IHello"})
+
+  -- Assume que HelloComponent é o único serviço cadastrado.
+  local helloComponent = orb:narrow(offers[1].member,
+                      "IDL:scs/core/IComponent:1.0")
+  local helloFacet = helloComponent:getFacet("IDL:demoidl/hello/IHello:1.0")
+  helloFacet = orb:narrow(helloFacet, "IDL:demoidl/hello/IHello:1.0")
+
+  -- Chama sayHello antes de desconectar.
+  helloFacet:sayHello()
+
+  openbus:disconnect()
+
+  -- Chama sayHello após desconectar. Dessa forma, se o metodo estiver
+  -- sendo checado, um erro será lançado pois não há credencial. O comportamento
+  -- esperado é que a chamada funcione normalmente.
+  helloFacet:sayHello()
+
+  openbus:destroy()
+end
+
+oil.main(function()
+  print(oil.pcall(main))
+end)
