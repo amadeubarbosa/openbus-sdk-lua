e50d82dfcd78d20430eef8f44b577bab8bf8aaca
Amadeu Andrade Babosa Junior <amadeu@tecgraf.puc-rio.br>
2011-07-29 20:18:44 +0000

    Incorporando a definicao das variaveis de ambiente para o OpenBus SDK Lua ser usado corretamente [OPENBUS-1372]. Essa mudanca passou a ser necessaria depois que eu removi a dependencia com o servicelauncher (usando direto lua5.1) de acordo com a issue [OPENBUS-653].
    Essa mudanca respeita a OPENBUS_DATADIR se ela estiver definida previamente.
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/lua/branches@120540 ae0415b3-e90b-0410-900d-d0be9363c56b

--- OB_SDK_LUA_v1_05_03_2011_07_28/test/run_unit_test.sh
+++ OB_SDK_LUA_v1_05_03_2011_07_28/test/run_unit_test.sh
@@ -1,5 +1,14 @@
 #!/bin/ksh
 
+OPENBUS_DATADIR=${OPENBUS_DATADIR:=$OPENBUS_HOME/data}
+export OPENBUS_DATADIR
+
+CONFIG=${OPENBUS_DATADIR}/conf/config
+
+if [ -f "${CONFIG}" ]; then
+  source ${CONFIG}
+fi
+
 PARAMS=$*
 
 LATT_HOME=${OPENBUS_HOME}/lib/lua/5.1/latt
