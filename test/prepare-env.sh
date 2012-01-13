5fd26939ec3ebb842f643c9229326335703356b0
Hugo Roenick <hroenick@tecgraf.puc-rio.br>
2012-01-13 13:20:55 +0000

    [OPENBUS-1599] Criar suite de testes para o core e sdk-lua do OpenBus 2.0  - removendo configuração de ambiente do teste  - craindo scripts para preparar e restaurar ambientes de teste
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/lua/branches/openbus_v2_proto@125701 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ test/prepare-env.sh
@@ -0,0 +1,62 @@
+#!/bin/ksh
+
+if [ -z "${OPENBUS_HOME}" ] ;then
+  echo "[ERRO] Variável de ambiente OPENBUS_HOME não definida"
+  exit 1
+fi 
+
+###############################################################################
+
+source ./test.properties
+
+if [ -z "${host}" ]; then
+  host="localhost"
+fi
+if [ -z "${port}" ]; then
+  port=2089
+fi
+if [ -z "${adminLogin}" ]; then
+  admimLogin="admin"
+fi
+if [ -z "${adminPassword}" ]; then
+  adminPassword="admin"
+fi
+if [ -z "${entity}" ]; then
+  entity="TesteBarramento"
+fi
+if [ -z "${category}" ]; then
+  category=${entity}
+fi
+if [ -z "${certificate}" ]; then
+  certificate="${entity}.crt"
+fi
+
+###############################################################################
+
+if [ ! -e ${certificate} ]; then
+  echo -e '\n\n\n\n\n\n\n' | openssl-generate.ksh -n ${entity}
+  CODE=$?
+  if [ ${CODE} -ne 0 ]; then
+    echo "[ERRO] Falha na geração dos certificados"
+    exit 1
+  fi
+fi
+
+ADMIN_EXTRAARGS="--host=${host} --port=${port} "
+ADMIN_EXTRAARGS="${ADMIN_EXTRAARGS} --login=${adminLogin} "
+ADMIN_EXTRAARGS="${ADMIN_EXTRAARGS} --password=${adminPassword} "
+
+${OPENBUS_HOME}/bin/busadmin ${ADMIN_EXTRAARGS} --add-category=${category} --name="Teste_do_OpenBus"
+CODE=$?
+${OPENBUS_HOME}/bin/busadmin ${ADMIN_EXTRAARGS} --add-entity=${entity} --category=${category} --name="Teste_do_Barramento"
+CODE=$?
+${OPENBUS_HOME}/bin/busadmin ${ADMIN_EXTRAARGS} --add-certificate=${entity} --certificate=${certificate}
+CODE=$?
+
+# hoje não é possível recuperar o código de retorno da execução do busadmin
+# essa verificação final precisa ser revista
+if [ ${CODE} -ne 0 ]; then
+  echo "[ERRO] Falha ao configurar o ambiente de teste."
+  exit 1
+fi
+
---
+++ test/restore-env.sh
@@ -0,0 +1,51 @@
+#!/bin/ksh
+
+if [ -z "${OPENBUS_HOME}" ] ;then
+  echo "[ERRO] Variável de ambiente OPENBUS_HOME não definida"
+  exit 1
+fi 
+
+###############################################################################
+
+source ./test.properties
+
+if [ -z "${host}" ]; then
+  host="localhost"
+fi
+if [ -z "${port}" ]; then
+  port=2089
+fi
+if [ -z "${adminLogin}" ]; then
+  admimLogin="admin"
+fi
+if [ -z "${adminPassword}" ]; then
+  adminPassword="admin"
+fi
+if [ -z "${entity}" ]; then
+  entity="TesteBarramento"
+fi
+if [ -z "${category}" ]; then
+  category=${entity}
+fi
+
+###############################################################################
+
+ADMIN_EXTRAARGS="--host=${host} --port=${port} "
+ADMIN_EXTRAARGS="${ADMIN_EXTRAARGS} --login=${adminLogin} "
+ADMIN_EXTRAARGS="${ADMIN_EXTRAARGS} --password=${adminPassword} "
+
+${OPENBUS_HOME}/bin/busadmin ${ADMIN_EXTRAARGS} --del-certificate=${entity}
+${OPENBUS_HOME}/bin/busadmin ${ADMIN_EXTRAARGS} --del-entity=${entity}
+${OPENBUS_HOME}/bin/busadmin ${ADMIN_EXTRAARGS} --del-category=${category}
+CODE=$?
+
+# hoje não é possível recuperar o código de retorno da execução do busadmin
+# essa verificação final precisa ser revista
+if [ ${CODE} -ne 0 ]; then
+  echo "[ERRO] Falha ao configurar o ambiente de teste."
+  exit 1
+fi
+
+# removendo certificados
+rm *.crt
+rm *.key
