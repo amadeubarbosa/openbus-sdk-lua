59e7e67c16d4916fee97e1395d31689cff8f6d76
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2012-06-25 16:34:07 +0000

    [OPENBUS-1599] Criar suite de testes para o core e sdk-lua do OpenBus 2.0
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/lua/branches@130303 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ openbus_v2_proto/test/runall.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CONSOLE="${OPENBUS_HOME}/bin/busconsole"
+TESTDIR=${OPENBUS_HOME}/test
+
+LUACASES="\
+openbus/util/tickets \
+openbus/util/database \
+openbus/LoginLogout \
+"
+for case in ${LUACASES}; do
+	echo -n "Test '${case}' ... "
+	${CONSOLE} ${TESTDIR}/${case}.lua $@
+	echo "OK"
+done
