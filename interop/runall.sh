1da9976b000f7eb5e3a5fbe034b8665c6519967b
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2014-12-09 18:08:52 +0000

    [OPENBUS-2553] [Lua] Implementar teste de interoperabilidade da serialização de cadeias
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/lua/branches/02_00_01@159698 ae0415b3-e90b-0410-900d-d0be9363c56b

--- interop/runall.sh
+++ interop/runall.sh
@@ -5,4 +5,5 @@
 /bin/bash runtest.sh $1 multiplexing server
 /bin/bash runtest.sh $1 sharedauth server
 /bin/bash runtest.sh $1 reloggedjoin server proxy
+/bin/bash runtest.sh $1 chaining server proxy
 /bin/bash runtest.sh $1 delegation messenger broadcaster forwarder
