9595a26b06566bc7588ce1d164f7d4f904892951
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2015-01-23 20:33:20 +0000

    [OPENBUS-2739] Permitir modo interativo do 'busconsole' e 'busadmin' - Ajustar o modelo de interoperabilidade dos testes de SDK-Lua x Core para acomodar eventuais mudanças na interface do 'busconsole' como aconteceu na versão 2.1.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/lua/branches/02_00_00@161222 ae0415b3-e90b-0410-900d-d0be9363c56b

--- test/runall.sh
+++ test/runall.sh
@@ -21,21 +21,24 @@ echo "login.lease.time=$leasetime"            >> $OPENBUS_TESTCFG
 echo "password.penalty.time=$passwordpenalty" >> $OPENBUS_TESTCFG
 #echo "openbus.test.verbose=yes"               >> $OPENBUS_TESTCFG
 
-source ${OPENBUS_CORE_TEST}/runbus.sh $mode BUS01 $bus1port
-source ${OPENBUS_CORE_TEST}/runbus.sh $mode BUS02 $bus2port
+runbus="${OPENBUS_CORE_TEST}/runbus.sh $mode"
+runadmin="${OPENBUS_CORE_TEST}/runadmin.sh $mode"
+
+source $runbus BUS01 $bus1port
+source $runbus BUS02 $bus2port
 genkey $OPENBUS_TEMP/testsyst
 
-source ${OPENBUS_CORE_TEST}/runadmin.sh $mode localhost $bus1port --script=${OPENBUS_CORE_TEST}/test.adm
-source ${OPENBUS_CORE_TEST}/runadmin.sh $mode localhost $bus2port --script=${OPENBUS_CORE_TEST}/test.adm
+source runadmin localhost $bus1port --script=${OPENBUS_CORE_TEST}/test.adm
+source runadmin localhost $bus2port --script=${OPENBUS_CORE_TEST}/test.adm
 source runtests.sh $mode
-source ${OPENBUS_CORE_TEST}/runadmin.sh $mode localhost $bus1port --undo-script=${OPENBUS_CORE_TEST}/test.adm
-source ${OPENBUS_CORE_TEST}/runadmin.sh $mode localhost $bus2port --undo-script=${OPENBUS_CORE_TEST}/test.adm
+source runadmin localhost $bus1port --undo-script=${OPENBUS_CORE_TEST}/test.adm
+source runadmin localhost $bus2port --undo-script=${OPENBUS_CORE_TEST}/test.adm
 
 cd ../interop
-source ${OPENBUS_CORE_TEST}/runadmin.sh $mode localhost $bus1port --script=script.adm
-source ${OPENBUS_CORE_TEST}/runadmin.sh $mode localhost $bus2port --script=script.adm
+source runadmin localhost $bus1port --script=script.adm
+source runadmin localhost $bus2port --script=script.adm
 source runall.sh $mode
 sleep 2 # wait for offers to expire
-source ${OPENBUS_CORE_TEST}/runadmin.sh $mode localhost $bus1port --undo-script=script.adm
-source ${OPENBUS_CORE_TEST}/runadmin.sh $mode localhost $bus2port --undo-script=script.adm
+source runadmin localhost $bus1port --undo-script=script.adm
+source runadmin localhost $bus2port --undo-script=script.adm
 cd ../test
